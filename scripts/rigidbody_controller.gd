835319c901b070eae24dad9c99746b6cce6a9887
Treikeh <73736974+Treikeh@users.noreply.github.com>
2024-12-09 14:05:52 +0100

    Movement in rigidbody controller is now working

--- scripts/rigidbody_controller.gd
+++ scripts/rigidbody_controller.gd
@@ -15,6 +15,10 @@ var gravity_direction: Vector3 = Vector3.DOWN
 var ground_normal: Vector3 = Vector3.UP
 var move_direction: Vector3 = Vector3.ZERO
 
+# Custom gravity. NOT IN USE
+var rot_speed: float = 5.0
+var rot_basis: Basis
+
 @export_group("Spring force")
 @export var rest_height: float = 1.0
 @export var ground_buffer: float = 0.5
@@ -56,14 +60,13 @@ func _input(event: InputEvent) -> void:
 		move_input = Input.get_vector("move_l", "move_r", "move_f", "move_b")
 
 func _process(delta: float) -> void:
-	is_grounded = ground_check.is_colliding()#is_on_walkable_slope()
-	print(is_grounded)
+	is_grounded = is_on_walkable_slope()
 	# Camera tilt and head bobbing
 	camera.apply_camera_tilt(linear_velocity, move_direction, delta)
 	if is_grounded and check_for_ground:
 		camera.head_bobbing(linear_velocity, delta)
 		gravity_scale = 0.1
-		ground_check.target_position = to_local(global_position + (gravity_direction * (rest_height * ground_buffer)))
+		ground_check.target_position = to_local(global_position + (gravity_direction * (rest_height + ground_buffer)))
 	else:
 		gravity_scale = 1.0
 		ground_check.target_position = to_local(global_position + (gravity_direction * rest_height))
@@ -72,6 +75,7 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if is_grounded:
+		ground_normal = ground_check.get_collision_normal()
 		var slope_dir: Vector3 = move_direction.slide(ground_normal)
 		var target_vel: Vector3 = slope_dir * max_speed
 		var needed_vel: Vector3 = target_vel - linear_velocity
@@ -81,7 +85,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		var target_vel: Vector3 = move_direction * max_speed
 		var needed_vel: Vector3 = target_vel - linear_velocity
+		# FIXME: Add normal gravity to needed_vel
 		apply_central_force(needed_vel * air_accel * delta * mass)
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	return # Remove this line for custom gravity support
+	@warning_ignore("unreachable_code")
+	var grav_vec: Vector3 = state.total_gravity.normalized()
+	if gravity_direction != grav_vec:
+		gravity_direction = grav_vec
+		var forward_dir: Vector3 = global_basis.z
+		var upwards_dir: Vector3 = -gravity_direction
+		var left_dir: Vector3 = upwards_dir.cross(forward_dir)
+		rot_basis = Basis(left_dir, upwards_dir, forward_dir).orthonormalized()
+	state.transform.basis = basis.slerp(rot_basis, rot_speed * state.step)
 
 func snap_to_ground(_delta: float) -> void:
 	var hit_distance: float = (ground_check.global_position - ground_check.get_collision_point()).length()
@@ -93,8 +110,7 @@ func snap_to_ground(_delta: float) -> void:
 func is_on_walkable_slope() -> bool:
 	if ground_check.is_colliding():
 		ground_normal = ground_check.get_collision_normal()
-		print(ground_normal.angle_to(-gravity_direction))
-		if ground_normal.angle_to(-gravity_direction) < max_slope_angle:
+		if ground_normal.angle_to(-gravity_direction) < deg_to_rad(max_slope_angle):
 			return true
 		else:
 			return false
