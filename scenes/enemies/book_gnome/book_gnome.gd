e7d2c88fe06a70f7c3a21817262cd07820112d19
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-01-10 12:54:18 +0100

    Added more assets to the book_gnome enemy

--- scenes/enemies/book_gnome/book_gnome.gd
+++ scenes/enemies/book_gnome/book_gnome.gd
@@ -6,9 +6,13 @@ extends Enemy
 @export var acceleration: float = 10.0
 var gravity_force: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var walking: bool = false
+var attacking: bool = false
+
 @export_group("Nodes")
 @export var mesh: Node3D
 @export var animation_player: AnimationPlayer
+@export var animation_tree: AnimationTree
 
 
 func _ready() -> void:
@@ -21,6 +25,12 @@ func _process(delta: float) -> void:
 	var player_node: Node3D = get_tree().get_first_node_in_group("player")
 	if !animation_player.is_playing() and is_instance_valid(player_node):
 		mesh.look_at(player_node.global_position)
+	
+	# ANIMATION
+	walking = false
+	print(velocity.length_squared())
+	if velocity.length_squared() > 1.0:
+		walking = true
 
 
 func _physics_process(delta: float) -> void:
@@ -34,12 +44,17 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity_force * delta
 	
-	if not navigation.is_target_reached() or receiving_knockback:
-		move_and_slide()
+	#FIXME: This is causing issues
+	#if not navigation.is_target_reached() or receiving_knockback:
+	move_and_slide()
 
 
 func _attack() -> void:
 	recive_knockback(-mesh.global_basis.z)
+	# ANIMATION
+	attacking = true
+	await get_tree().create_timer(0.5).timeout
+	attacking = false
 
 
 #region Health
@@ -49,6 +64,6 @@ func _on_health_depleted() -> void:
 	behavior_tree.active = false
 	# Stop navigation agent
 	navigation.target_position = global_position
-	animation_player.play("died")
+	animation_player.play("died_001")
 
 #endregion
