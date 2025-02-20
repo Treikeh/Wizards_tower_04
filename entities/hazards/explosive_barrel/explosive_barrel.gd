8e2fd060068e9b2e884653f74e108dbf7dc58fea
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-02-20 16:31:59 +0100

    Removed the explosion delay on the explosive barrel

--- entities/hazards/explosive_barrel/explosive_barrel.gd
+++ entities/hazards/explosive_barrel/explosive_barrel.gd
@@ -3,18 +3,9 @@ extends Node3D
 
 @export_group("Nodes")
 @export var explosion_cast: ShapeCast3D
-@export var health_area: HealthArea3D
 @export var camera_shake_source: Area3D
-@export var explosion_delay: Timer
 
 
-func _on_health_health_depleted() -> void:
-	# A small delay before the barrel explodes to avoid having every barrel in its radius explode ->
-	# <- at the same time
-	explosion_delay.start(0.0)
-
-
-func _on_explosion_delay_timeout() -> void:
+func _on_health_depleted() -> void:
 	explosion_cast.trigger()
 	camera_shake_source.shake_camera()
-	queue_free()
