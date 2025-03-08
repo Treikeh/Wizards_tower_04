fc6f8b636da437997606cf5cc0a3139f26a7e1bf
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-03-08 17:56:35 +0100

    Worked on levels

--- levels/level_hub/level_hub.gd
+++ levels/level_hub/level_hub.gd
@@ -1,19 +1,10 @@
 extends Node3D
 
 
-@export_file("*.tscn") var spell_select_menu_scene: String
-
-
 func _ready() -> void:
 	Globals.reset_run_info()
 
 
 func _on_play_trigger_entered() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	var menu: Control = UiManager.add_ui_scene(spell_select_menu_scene)
-	menu.tree_exited.connect(_on_run_started)
-
-
-func _on_run_started() -> void:
-	Globals.start_run()
 	Globals.load_random_level()
+	Globals.start_run()
