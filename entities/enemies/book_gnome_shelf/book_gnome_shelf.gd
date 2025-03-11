0b60ef01ab6f40bf3db416de7d237dcaa5c5e49d
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-03-11 17:46:39 +0100

    Fixed target_direction.look_at() bug

--- entities/enemies/book_gnome_shelf/book_gnome_shelf.gd
+++ entities/enemies/book_gnome_shelf/book_gnome_shelf.gd
@@ -12,12 +12,11 @@ var book_gnomes_spawned: int = 0
 var book_gnome_scene: String = "uid://uvws4xru1kpl"
 
 
-func _physics_process(delta: float) -> void:
-	super(delta)
+func _process(_delta: float) -> void:
 	# I don't like this. Don't know why, but i just don't like its
 	var horizontal_vel: Vector3 = Vector3(velocity.x, 0.0, velocity.z)
-	if horizontal_vel:
-		target_direction.look_at(global_position + horizontal_vel.normalized())
+	if horizontal_vel.round() != Vector3.ZERO:
+		target_direction.look_at(target_direction.global_position + horizontal_vel)
 
 
 func attack() -> void:
