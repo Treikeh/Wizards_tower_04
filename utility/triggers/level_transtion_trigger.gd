bd08c5093a8b119cda9aa42cd22506bbf82a63e7
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-04-13 15:19:35 +0200

    Can choose to transition to specific levels/maps when walking into a level_transition_trigger

--- utility/triggers/level_transtion_trigger.gd
+++ utility/triggers/level_transtion_trigger.gd
@@ -1,6 +1,12 @@
 extends Area3D
 
 
+@export_file("*.tscn") var level: String = ""
+
+
 func _on_body_entered(_body: Node3D) -> void:
-	Globals.rooms_cleared += 1
-	Globals.load_next_level()
+	if level == "":
+		Globals.rooms_cleared += 1
+		Globals.load_next_level()
+	else:
+		LevelManager.change_level(level)
